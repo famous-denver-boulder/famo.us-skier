acd492c96ef93f2563ac4e9b01f9936c2a31e405
Lee <leeblazek@gmail.com>
2014-11-03 23:30:21 -0700

    added click to animate body

--- app/scripts/main/main-ctrl.js
+++ app/scripts/main/main-ctrl.js
@@ -1,10 +1,13 @@
 'use strict';
 
 angular.module('famousAngularStarter')
-  .controller('MainCtrl', function($scope, $famous) {
+  .controller('MainCtrl', function($scope, $famous, $timeline) {
     var Transitionable = $famous['famous/transitions/Transitionable'];
     var Timer = $famous['famous/utilities/Timer'];
+    var Easing = $famous['famous/transitions/Easing'];
 
+
+    $scope.align = new Transitionable([.5, .5, 0]);
 
     $scope.spinner = {
       speed: 55
@@ -17,12 +20,20 @@ angular.module('famousAngularStarter')
       rad: 0
     };
 
+
+    //famous does rotation in radians this converts angle # to radians
     $scope.degToRad = function() {
       $scope.slopeRad.rad = $scope.slope.angle * 3.14159265359 / 180;
 
     };
     $scope.rotateY = new Transitionable(0);
 
+    $scope.slide = function() {
+      $scope.align.set([2, .5, 1], {
+        duration: 5000,
+        curve: 'easeInOut'
+      });
+    };
     //run function on every tick of the Famo.us engine
     Timer.every(function() {
       var adjustedSpeed = parseFloat($scope.spinner.speed) / 1200;
